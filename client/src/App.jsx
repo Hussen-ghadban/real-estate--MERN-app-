247f334f1c8095e8b9b019af5d514d42f92e6b24
Hessen <houssein.ghadban99@gmail.com>
2023-12-08 15:26:26 +0200

    add backend

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/sign-in' element={<Signin/>}></Route>
+        <Route path='/sign-up' element={<SignUp/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
